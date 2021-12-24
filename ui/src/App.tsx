1f92612a129dd33f3331b5efc42c1e621cc2e784
Bailey Everts <texas312@gmail.com>
2021-12-24 10:40:30 -0700

    feat: add hello world function, print balance

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {  ethers } from 'ethers';
+
+declare global {
+  interface Window {
+    ethereum: any;
+  }
+}
+
+const address = 'everts.eth';
 
 function App() {
+  if ('ethereum' in window) {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    // const signer = provider.getSigner()
+    provider.getBalance(address).then(balance => alert(`${address} contains ${ethers.utils.formatEther(balance)} ETH`));
+  } else {
+    alert('no bueno')
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Connect a wallet to get started!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
