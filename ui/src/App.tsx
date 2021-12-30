b917eda0493b6e3115d55d34e4baa71918b4cc9c
Bailey Everts <texas312@gmail.com>
2021-12-30 10:10:37 -0700

    lint wip

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
+import { ethers } from 'ethers';
 import logo from './logo.svg';
 import './App.css';
 import { getChallenge, getJwt } from './sdk';
-import {  ethers } from 'ethers';
-
 
 declare global {
+  // eslint-disable-next-line no-unused-vars
   interface Window {
     ethereum: any;
   }
 }
 
-
 async function login() {
   const provider = new ethers.providers.Web3Provider(window.ethereum);
-  await provider.send("eth_requestAccounts", []);
+  await provider.send('eth_requestAccounts', []);
   const signer = provider.getSigner();
   const address = await signer.getAddress();
   const challange = await getChallenge(address);
@@ -25,9 +24,9 @@ async function login() {
 
 function App() {
   if ('ethereum' in window) {
-    login()
+    login();
   } else {
-    alert('please use a web3 compatible browser')
+    alert('please use a web3 compatible browser');
   }
 
   return (
